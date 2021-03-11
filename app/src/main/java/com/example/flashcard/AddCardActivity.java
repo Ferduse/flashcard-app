d8f7aee567cb8c9a00abaa515a3e161cc69c112e
Ferduse <ferduse.rahman79@myhunter.cuny.edu>
2021-03-10 20:53:09 -0500

    Lab 2 Completed

---
+++ app/src/main/java/com/example/flashcard/AddCardActivity.java
@@ -0,0 +1,47 @@
+package com.example.flashcard;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+
+        findViewById(R.id.cancelBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        findViewById(R.id.saveBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String card_q = ((EditText) findViewById(R.id.questionPlace)).getText().toString();
+                String card_a = ((EditText) findViewById(R.id.answerPlace)).getText().toString();
+
+
+                Intent data = new Intent();
+                data.putExtra("question", card_q);
+                data.putExtra("answer", card_a);
+                setResult(RESULT_OK, data);
+                finish();
+            }
+
+        });
+
+        String s1 = getIntent().getStringExtra("question");
+        String s2 = getIntent().getStringExtra("answer");
+
+
+
+    }
+}
+
--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.flashcard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         TextView choiceThree = findViewById(R.id.choiceThree);
         ImageView toggle = findViewById(R.id.toggle);
         Button buttonR = findViewById(R.id.buttonR);
+        ImageView add = (ImageView) findViewById(R.id.add);
 
         //for the button.
         final boolean[] showAnswers = {true};
@@ -79,6 +81,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
         // to show the choices and hide the choices
         toggle.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -101,5 +105,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.add).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+
+        });
+
+        Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+        intent.putExtra("question", " ");
+        intent.putExtra("answer", " ");
+        MainActivity.this.startActivityForResult(intent, 100);
+
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) { // this 100 needs to match the 100 we used when we called startActivityForResult!
+            String question = data.getExtras().getString("question"); // 'string1' needs to match the key we used when we put the string in the Intent
+            String answer = data.getExtras().getString("answer");
+
+            ((TextView) findViewById(R.id.question)).setText(question);
+            ((TextView) findViewById(R.id.answer)).setText(answer);
+        }
+    }
+
 }
